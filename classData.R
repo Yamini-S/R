4f6c7b2133737bf6be226e2ceba861705c1f6649
Yamini-S <sehrawat.y@husky.neu.edu>
2017-02-03 20:57:27 -0500

    R file

---
+++ classData.R
@@ -0,0 +1,17 @@
+
+#-----------Class Data---------------------------------------------------------------------------
+
+#----Min Max, Avg, Median GPA---
+minGPA <- min(classData$GPA)
+print (minGPA)
+
+maxGPA <- max(classData$GPA)
+print(maxGPA)
+
+avgGPA <- mean(classData$GPA)
+print(avgGPA);
+
+medianGPA <- median(classData$GPA)
+print(medianGPA)
+
+
